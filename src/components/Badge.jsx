70efd2c2960677b805e9963ce56a8df1f2e0eeec
ChristianTC <christiantambo15@gmail.com>
2021-01-22 19:37:21 -0400

    style in component Badge, also Bootstrap has been installed and imported in index.js

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,25 +2,30 @@ import React, { Fragment } from 'react';
 
 import confLogo from '../images/badge-header.svg';
 
+import './styles/Badge.css';
+
 class Badge extends React.Component {
     render() {
         return (
-            <Fragment>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="Conference Logo" />
                 </div>
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+                <div className="Badge__section-name">
+                    <img 
+                        className="Badge__avatar" 
+                        src="https://www.gravatar.com/avatar?d=identicon" 
+                        alt="Avatar"/>
                     <h1>
                         Christian <br/> Tambo
                     </h1>
                 </div>
-                <div>
-                    <p>Frontend Developer</p>
-                    <p>@ChristianTambo</p>
-                    <p>#PlatziConf</p>
+                <div className="Badge__section-info">
+                    <h1>Frontend Developer</h1>
+                    <div>@ChristianTambo</div>
                 </div>
-            </Fragment>
+                <div className="Badge__footer">#PlatziConf</div>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './global.css';
 import Badge from './components/Badge';
+
 
 
 const container = document.getElementById('app');
