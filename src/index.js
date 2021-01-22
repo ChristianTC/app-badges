2a123f56b415bf8018616a7c1ba422cb41c398ea
ChristianTC <christiantambo15@gmail.com>
2021-01-22 18:30:49 -0400

    JSX

--- src/index.js
+++ src/index.js
@@ -8,9 +8,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const element = <h1>Hello, Platzi Badges!</h1>;
 
+// React.create.element TEST
+// const element = React.createElement(
+//     'a',
+//     { href: 'https://platzi.com'}, 
+//     'Children'
+//     );
+
+const NAME = 'Christian';
+const jsx = (
+    <div>
+        <h1>Hola, soy {NAME}</h1>
+        <p>I'm frontend developer</p>
+    </div>
+);
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+// ReactDOM.render(__what__, __where__);
+ReactDOM.render(jsx, container);
