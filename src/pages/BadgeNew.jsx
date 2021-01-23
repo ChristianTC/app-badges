88f4d538da0b7c2a27e0a37f1bab171aaa5d0db6
ChristianTC <christiantambo15@gmail.com>
2021-01-23 00:14:50 -0400

    modifying the state in BadgeNew

--- src/pages/BadgeNew.jsx
+++ src/pages/BadgeNew.jsx
@@ -7,6 +7,25 @@ import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
 
 class BadgeNew extends React.Component {
+    state = { form: {
+        firstname: '',
+        lastname: '',
+        jobTitle: '',
+        email: '',
+        twitter: '',
+    } };
+
+    handleChange = e => {
+        // const nextForm = this.state.form;
+        // nextForm[e.target.name] = e.target.value;
+        this.setState({
+            form: { 
+                ...this.state.form,
+                [e.target.name]: e.target.value,
+            },
+        });
+    }
+
     render() {
         return (
             <div>
@@ -19,15 +38,18 @@ class BadgeNew extends React.Component {
                     <div className="row">
                         <div className="col-6">
                             <Badge 
-                                firstName="Christian" 
-                                lastName="Tambo" 
-                                twitter="@christiantambo" 
-                                jobTitle="Frontend Developer" 
+                                firstName={this.state.form.firstName || "Christian"} 
+                                lastName={this.state.form.lastName || "Tambo"} 
+                                twitter={this.state.form.twitter || "@christiantambo"} 
+                                jobTitle={this.state.form.jobTitle || "Frontend Developer"} 
                                 avatarUrl="https://www.gravatar.com/avatar/27729f4e2e143f42dba6b578487e3788=https%3A%2F%2Fexample.com%2Fimages%2Favatar.jpg" 
                             />
                         </div>
                         <div className="col-6">
-                            <BadgeForm />
+                            <BadgeForm 
+                                onChange={this.handleChange} 
+                                formValues={this.state.form} 
+                            />
                         </div>
                     </div>
                 </div>
