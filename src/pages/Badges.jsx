cf39045f08e251db38cfedf0cd81ca651eb3300b
ChristianTC <christiantambo15@gmail.com>
2021-01-23 20:53:15 -0400

    improving UX during a petition

--- src/pages/Badges.jsx
+++ src/pages/Badges.jsx
@@ -5,6 +5,8 @@ import './styles/Badges.css';
 import { Link } from 'react-router-dom';
 
 import api from '../api';
+import PageLoading from '../components/PageLoading';
+import PageError from '../components/PageError';
 
 class Badges extends React.Component{
     state = {
@@ -38,10 +40,15 @@ class Badges extends React.Component{
 
     render() {
         if(this.state.loading === true ) {
-            return 'Loading';
+            return (
+                <PageLoading />
+            )
         }
         if(this.state.error) {
-            return `Error: ${this.state.error.message}`;
+            // return `Error: ${this.state.error.message}`;
+            return (
+                <PageError error={this.state.error}/>
+            )
         }
         return (
             <React.Fragment>
